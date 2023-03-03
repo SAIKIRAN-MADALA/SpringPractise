4a66003eae3650714cf67f3e232df2f7493dc220
Sai Kiran <saikiranmadala10@gmail.com>
2023-03-02 20:51:14 -0800

    added create controller

--- ProjectInfoGraphics/src/main/java/com/product/ProjectInfoGraphics/ProjectInfoGraphicsApplication.java
+++ ProjectInfoGraphics/src/main/java/com/product/ProjectInfoGraphics/ProjectInfoGraphicsApplication.java
@@ -2,14 +2,8 @@ package com.product.ProjectInfoGraphics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.product.ProjectInfoGraphics.controller")
-@ComponentScan(basePackages = "com.product.ProjectInfoGraphics.model")
-@ComponentScan(basePackages = "com.product.ProjectInfoGraphics.entity")
-@ComponentScan(basePackages = "com.product.ProjectInfoGraphics.service")
-@ComponentScan(basePackages = "com.product.ProjectInfoGraphics.repository")
 public class ProjectInfoGraphicsApplication {
 
 	public static void main(String[] args) {
